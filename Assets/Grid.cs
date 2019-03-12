9c6a4fadca03df19a01a8a85cbfea675a39d624f
Jen-Hobbs <jen.g.hobbs@gmail.com>
2019-03-12 10:05:24 -0700

    transfering computers

---
+++ Assets/Grid.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Grid : MonoBehaviour
+{
+    public Tilemap tilemap;
+    TileBase tile;
+    TileBase[] allTiles;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        BoundsInt bounds = tilemap.cellBounds;
+        allTiles = tilemap.GetTilesBlock(bounds);
+
+        for (int x = 0; x < bounds.size.x; x++)
+        {
+            for (int y = 0; y < bounds.size.y; y++)
+            {
+                TileBase tile = allTiles[x + y * bounds.size.x];
+                if (tile != null)
+                {
+                    Debug.Log("x:" + x + " y:" + y + " tile:" + tile.name);
+                }
+                else
+                {
+                    Debug.Log("x:" + x + " y:" + y + " tile: (null)");
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 pos = Camera.main.ScreenToWorldPoint((Vector2)Input.mousePosition);
+            
+            Debug.Log(Camera.main.ScreenToWorldPoint((Vector2)Input.mousePosition));
+        }
+    }
+}
