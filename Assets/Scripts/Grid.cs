d31b1cf416d7ba02a5afe645fe13e45f38b78ec8
Jen-Hobbs <jen.g.hobbs@gmail.com>
2019-03-18 21:55:57 -0700

    object selection by mouse prototype

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -7,14 +7,14 @@ public class Grid : MonoBehaviour
 {
     public Tilemap tilemap;
     public GridLayout world;
-    TileBase tile;
+    public TileBase tile;
     TileBase[] allTiles;
 
     public GameObject bluehouse;
     // Start is called before the first frame update
     void Start()
     {
-
+        /*
         BoundsInt bounds = tilemap.cellBounds;
         allTiles = tilemap.GetTilesBlock(bounds);
 
@@ -33,6 +33,7 @@ public class Grid : MonoBehaviour
                 }
             }
         }
+        */
     }
 
     // Update is called once per frame
@@ -45,7 +46,13 @@ public class Grid : MonoBehaviour
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             Vector3 worldPoint = ray.GetPoint(-ray.origin.z / ray.direction.z);
             Debug.Log(world.WorldToCell(worldPoint));
-            GameObject tile = Instantiate(bluehouse, worldPoint, Quaternion.identity);
+            //GameObject tile = Instantiate(bluehouse, worldPoint, Quaternion.identity);
+
+            //tilemap.SetTile(new Vector3Int((int)worldPoint.x, (int)worldPoint.y, (int)worldPoint.z), null);
+            
+            var tilePos = world.WorldToCell(worldPoint);
+            print("test" + tilePos);
+            tilemap.SetTile(new Vector3Int((int)worldPoint.x, (int)worldPoint.y, (int)worldPoint.z), tile);
             /*
             Vector3 pos = Camera.main.ScreenToWorldPoint((Vector3)Input.mousePosition);
             Vector3Int position = new Vector3Int((int)pos.x, (int)pos.y, (int)pos.z);
---
+++ Assets/map.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//static class within grid that stores all objects
+public class map : MonoBehaviour
+{
+    public GridLayout world;
+    public GameObject check;
+    private GameObject[] checks;
+    // Start is called before the first frame update
+    void Start()
+    {
+        checks = GameObject.FindGameObjectsWithTag("house");
+        check = GameObject.FindWithTag("house");
+        if(checks.Length == 0)
+        {
+            Debug.Log("error");
+        }
+        Debug.Log(checks.Length);
+        Debug.Log(world.WorldToCell(checks[0].transform.position));
+        //Debug.Log(world.WorldToCell(check.transform.position));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldPoint = ray.GetPoint(-ray.origin.z / ray.direction.z);
+            Debug.Log(world.WorldToCell(check.transform.position));
+            Debug.Log(world.WorldToCell(worldPoint));
+        }
+    }
+}
